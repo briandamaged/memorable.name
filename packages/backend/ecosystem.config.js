2ca652841f27da95445f298c7697068da00ae2ef
Brian Lauber <constructible.truth@gmail.com>
2019-11-23 16:18:35 -0500

    Using pm2@3.5.1 to address a bug in the "interpreter" flag

--- packages/backend/ecosystem.config.js
+++ packages/backend/ecosystem.config.js
@@ -1,15 +1,19 @@
+
+const path = require('path');
+
+const INTERPRETER_PATH = path.resolve(__dirname, "node_modules/.bin/babel-node");
+
 module.exports = {
   apps : [{
     name: 'backend',
-    interpreter: './node_modules/.bin/babel-node',
-    interpreter_args: ['--extensions=.js,.ts'],
+    interpreter: INTERPRETER_PATH,
+    interpreter_args: ["--extensions=.js,.ts"],
     script: './src',
 
-
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
   }],
-
 };
