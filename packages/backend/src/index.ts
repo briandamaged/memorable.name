645c797dcd01164f12a44b45cd636e373b5a10e4
Brian Lauber <constructible.truth@gmail.com>
2019-11-23 16:41:23 -0500

    Basic scaffolding for koa-router

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,11 +1,31 @@
 
 import Koa from 'koa';
+import Router from 'koa-router';
 
 const app = new Koa();
+const rtr = new Router();
 
-app.use(function(ctx) {
-  ctx.body = "Howdy!";
+rtr.get('/', function(ctx) {
+  ctx.body = "Navigate to /api/names for 'Memorable' names!";
 });
+
+rtr.get('/api/names', function(ctx) {
+  ctx.body = {
+    content: [
+      {
+        first: "Warren",
+        last: "Peace",
+      },
+      {
+        first: "Helen",
+        last: "Wheels",
+      },
+    ]
+  };
+
+});
+
+app.use(rtr.routes());
 
 app.listen(3000, function() {
   console.log("Listening...");
