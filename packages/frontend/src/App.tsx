4953c27f7896a0e1ac4ca4d7c340318594cba1ce
Brian Lauber <constructible.truth@gmail.com>
2019-12-03 19:19:39 -0500

    Minimial integration w/ @apollo/react-hooks

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+});
+
+const query = gql`
+  {
+    names {
+      first
+      last
+    }
+  }
+`;
+
+
+const NamesList = ()=> {
+  const { data, loading, error } = useQuery(query);
+
+  if(loading) {
+    return <div>Loading...</div>;
+  };
+
+  if(error) {
+    return <div>Error!</div>;
+  }
+
+  return (
+    <ul>
+      {
+        data.names.map(({first, last}: {first: string, last: string}) => (
+          <li>{first} {last}</li>
+        ))
+      }
+    </ul>
+  );
+}
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client} >
+      <NamesList />
+    </ApolloProvider>
   );
 }
 
