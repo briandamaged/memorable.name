7af09f8b8b36430de79447563cdf9835dc938c8a
Brian Lauber <constructible.truth@gmail.com>
2019-12-09 20:53:02 -0500

    GraphQL resolvers interface w/ models now

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -17,87 +17,6 @@ const knex = Knex(require('../knexfile'));
 const models = createModels(knex);
 
 
-async function getGivenNames({id}: {id?: number | number[]} = {}) {
-  let query = knex.select().from('given_names_spellings');
-  if(typeof(id) === 'number') {
-    query = query.where('given_name_id', id);
-  } else if(Array.isArray(id)) {
-    query = query.where('given_name_id', id);
-  }
-
-  const gns = await query;
-  const groupings: Record<number, GivenName> = {};
-  for(const {given_name_id, spelling} of gns) {
-    const givenName = groupings[given_name_id] = (groupings[given_name_id] || {id: given_name_id, spellings: []});
-    givenName.spellings.push(spelling);
-  }
-
-  return Object.values(groupings);
-}
-
-
-async function getSurnames({id}: {id?: number | number[]} = {}) {
-  let query = knex.select().from('surnames_spellings');
-  if(typeof(id) === 'number') {
-    query = query.where('surname_id', id);
-  } else if(Array.isArray(id)) {
-    query = query.whereIn('surname_id', id);
-  }
-
-  const sns = await query;
-  const groupings: Record<number, Surname> = {};
-  for(const {surname_id, spelling} of sns) {
-    const surname = groupings[surname_id] = (groupings[surname_id] || {id: surname_id, spellings: []});
-    surname.spellings.push(spelling);
-  }
-
-  return Object.values(groupings);
-}
-
-
-interface ExtendedFullName extends FullName {
-  [key: string]: any
-}
-
-async function getFullNames() {
-  let givenNamesQuery = knex.select().from('full_names_given_names').orderBy('ordering', 'asc');
-  let surnamesQuery = knex.select().from('full_names_surnames').orderBy('ordering', 'asc');
-
-  const [fngns, fnsns] = await Promise.all([
-    givenNamesQuery, surnamesQuery,
-  ]);
-  
-  const groupings: Record<number, ExtendedFullName> = {};
-
-  for(const {full_name_id, given_name_id} of fngns) {
-    const fullName = groupings[full_name_id] = (groupings[full_name_id] || {
-      id: full_name_id,
-      given_name_ids: [],
-      givenNames: [],
-      surname_ids: [],
-      surnames: []
-    });
-
-    fullName.given_name_ids.push(given_name_id);
-  }
-
-
-  for(const {full_name_id, surname_id} of fnsns) {
-    const fullName = groupings[full_name_id] = (groupings[full_name_id] || {
-      id: full_name_id,
-      given_name_ids: [],
-      givenNames: [],
-      surname_ids: [],
-      surnames: []
-    });
-
-    fullName.surname_ids.push(surname_id);
-  }
-
-  return Object.values(groupings);
-}
-
-
 const schema = gql `
   type Query {
     fullNames: [FullName!]!
@@ -122,32 +41,12 @@ const schema = gql `
 const resolvers = {
   Query: {
     async fullNames(/* parent, args, context, info */) {
-      return getFullNames();
+      return models.FullName.fetchAll();
     },
 
     async givenNames() {
-      return getGivenNames();
+      return models.GivenName.fetchAll();
     },
-  },
-
-  FullName: {
-    async givenNames(parent: ExtendedFullName) {
-      const givenNames = await getGivenNames({id: parent.given_name_ids});
-      return givenNames;
-    },
-
-    async surnames(parent: ExtendedFullName) {
-      const surnames = await getSurnames({id: parent.surname_ids});
-      return surnames;
-    },
-  },
-
-  GivenName: {
-
-  },
-
-  Surname: {
-
   },
 };
 
