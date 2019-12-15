0b30b0d4a0724257a167c3b3a587771177614a80
Brian Lauber <constructible.truth@gmail.com>
2019-12-14 20:50:30 -0500

    In-progress: Integrating Storybook...
    ... however, the docker-compose.dev.yml file has been hacked
    slightly so that we can see the storybook stuffs.  We'll need
    to fix this in a future commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,24 +5,25 @@ const BABEL_NODE = path.resolve(__dirname, "node_modules/.bin/babel-node");
 
 module.exports = {
   apps : [
-    {
-      name: 'backend',
-      interpreter: BABEL_NODE,
-      interpreter_args: ["--extensions=.js,.ts"],
-      cwd: './packages/backend',
-      script: './src',
+    // {
+    //   name: 'backend',
+    //   interpreter: BABEL_NODE,
+    //   interpreter_args: ["--extensions=.js,.ts"],
+    //   cwd: './packages/backend',
+    //   script: './src',
 
-      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-    },
+    //   // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: false,
+    //   max_memory_restart: '1G',
+    // },
     {
       name: 'frontend',
       cwd: './packages/frontend',
       script: 'npm',
-      args: ['run', '--', 'start'],
+      // args: ['run', '--', 'start'],
+      args: ['run', '--', 'storybook'],
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -30,20 +31,20 @@ module.exports = {
       watch: false,
       max_memory_restart: '1G',
     },
-    {
-      name: 'types',
-      cwd: './packages/types',
-      script: 'npm',
-      args: ['run', '--', 'watch'],
+    // {
+    //   name: 'types',
+    //   cwd: './packages/types',
+    //   script: 'npm',
+    //   args: ['run', '--', 'watch'],
 
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      ignore_watch: [
-        "node_modules",
-        "lib",
-      ]
-    },
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: false,
+    //   max_memory_restart: '1G',
+    //   ignore_watch: [
+    //     "node_modules",
+    //     "lib",
+    //   ]
+    // },
   ],
 };
