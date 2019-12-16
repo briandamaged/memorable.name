2576c906f8874bb230462c72c1fb7b6fca3dcfaf
Brian Lauber <constructible.truth@gmail.com>
2019-12-16 12:29:59 -0500

    Dockerfile.dev uses pm2 instead of pm2-dev ...
    ... This way, we don't end up w/ several processes that are all
    watching the same files.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
       autorestart: true,
-      watch: false,
+      watch: true,
       max_memory_restart: '1G',
     },
     {
